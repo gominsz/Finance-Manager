f138f8b8bb0019f115e039e0da61debf270b1c63
Matheus Camargo <matheuscamargodealencar@gmail.com>
2024-05-28 16:55:26 -0300

    list items = items

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2023, 5, 13),
+    category: "food",
+    title: "MacDonald",
+    value: 32.12,
+  },
+  {
+    date: new Date(2023, 9, 13),
+    category: "food",
+    title: "BurguerKing",
+    value: 12,
+  },
+  {
+    date: new Date(2023, 10, 13),
+    category: "food",
+    title: "Atacadão",
+    value: 55,
+  },
+  {
+    date: new Date(2023, 5, 13),
+    category: "food",
+    title: "Fritinho",
+    value: 140.12,
+  },
+];
