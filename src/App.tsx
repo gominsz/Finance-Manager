b8c46940dabace9a5f0f5fbb686e452b0afea0c0
Matheus Camargo <matheuscamargodealencar@gmail.com>
2024-05-28 16:57:05 -0300

    app error list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { useState, useEffect } from 'react';
 import * as C from './app.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
 
 
 const App = () => {
+  const[list, setList] = useState(items);
+  const[filteredList, setFilteredList] = useState<Item[]>([]);
+  const[currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList( filterListByMonth(list, currentMonth );
+  }, [list, currentMonth]);
+
+
   return (
     <C.Container>
-      <h1>Hello World</h1>
+      <C.Headers>
+        <C.HeaderText>
+            Sistema financeiro
+        </C.HeaderText>
+      </C.Headers>
+      <C.Body>
+        <h1>Olá, mundo!</h1>
+        <h2>Bem vindo ao sistema financeiro</h2>
+
+        {/**AREA DE INFORMAÇOES */}
+
+        {/**AREA DE INCERÇÃO */}
+
+        {/**tABELA DE ITENS */}
+        
+
+
+      </C.Body>
     </C.Container>
   )
 }
